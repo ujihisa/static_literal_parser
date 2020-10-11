20ea3c2bbdfaea5e2952f303863633c9db45b1b7
ujihisa <ujihisa@users.noreply.github.com>
2020-10-10 21:50:39 -0700

    Initial import!
    * The hardest part was to name this gem properly.
    * It took about a week to name this.

---
+++ lib/static_literal_parser.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module StatisLiteralParser
+  def self.parse(str, constants)
+    parse_(RubyVM::AbstractSyntaxTree.parse(str), constants)
+  end
+
+  private_class_method def self.parse_(node, constants)
+    case node.type
+    when :SCOPE
+      parse_(node.children[2], constants)
+    when :LIST, :ZLIST
+      node.children[..-2].map { parse_(_1, constants) }
+    when :HASH
+      Hash[*parse_(node.children[0], constants)]
+    when :LIT, :STR
+      node.children[0]
+    when :CONST
+      constants[node.children[0]] or raise "Missing const: #{node.children[0]}"
+    when :COLON2
+      (parent, child) = node.children
+      case parent.type
+      when :CONST, :COLON2
+        parent = parse_(parent, constants)
+        constants[:"#{parent}::#{child}"] or raise "Missing nested const: #{[parent, child]}"
+      else
+        raise "Unexpected COLON2 parent #{parent.type}"
+      end
+    else
+      raise "Unexpected node #{node.type}"
+    end
+  end
+end
---
+++ static_literal_parser.gemspec
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Gem::Specification.new do |s|
+  s.name        = 'static_literal_parser'
+  s.version     = '0.0.1'
+  s.date        = '2020-10-10'
+  s.summary     = "summary"
+  s.description = "description"
+  s.authors     = ['Tatsuhiro Ujihisa']
+  s.email       = 'ujihisa at gmail com'
+  s.files       = ['lib/static_literal_parser.rb']
+  s.homepage    = 'https://github.com/ujihisa/static_literal_parser'
+  s.license     = 'GPL-3.0-or-later'
+end
